60cf290bd0fff0819926fb4a80709c81637921ea
João Pedro <joaoxstone@gmail.com>
2025-06-16 22:59:02 -0300

    constants

---
+++ constants/my-experiences.constant.js
@@ -0,0 +1,14 @@
+
+
+export const MyExperiences = [
+    {
+        date: '2023 - Presente',
+        position: 'Desenvolvedor Full Stack',
+        company: 'Izatec',
+    },
+    {
+        date: '2024 - 2025',
+        position: 'Desenvolvedor Frontend',
+        company: 'AML Reputacional',
+    },
+]
---
+++ constants/my-projects.constant.js
@@ -0,0 +1,32 @@
+import izapay from 'public/izapay.png';
+import izatecplay from 'public/izatecplay.png';
+
+export const MyProjects = [
+    {
+        name: 'IzaPlay',
+        externalUrl: 'https://izapay.com.br',
+        image: izatecplay,
+        stacks: [
+            'Nuxt',
+            'Nuxt UI',
+            'Tailwind CSS',
+            'TypeScript',
+            'SSR'
+        ],
+    },
+    {
+        name: 'IzaPay',
+        externalUrl: 'https://izapay.com.br',
+        github: 'https://github.com/izatecbr/org-izatec-pay-web',
+        image: izapay,
+        stacks: [
+            'Vue.js',
+            'Shadcn UI',
+            'Pinia',
+            'TypeScript',
+            'Spring Boot',
+            'Java',
+            'PostgreSQL',
+        ],
+    },
+];
