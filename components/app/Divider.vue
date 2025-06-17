a6e34fa537ab1a5eb5722b227c2da60ec55c8f2f
João Pedro <joaoxstone@gmail.com>
2025-06-16 22:59:25 -0300

    base components

--- components/app/Divider.vue
+++ components/app/Divider.vue
@@ -1,19 +1,42 @@
 <template>
-  <div class="divider" >
-    
-  </div>
+  <div
+    :class="['divider', _orientation]"
+    :style="{
+      backgroundColor: _color,
+      height: _orientation === 'horizontal' ? _height : '100%',
+      width: _orientation === 'horizontal' ? '100%' : _height,
+    }"
+  ></div>
 </template>
 
 <script lang="ts" setup>
+const {height, color, orientation} = defineProps<{
+  height?: string; 
+  color?: string; 
+  orientation?: 'horizontal' | 'vertical'; 
+}>();
 
+const defaultHeight = '1px';
+const defaultColor = 'var(--surface-subtle)';
+const defaultOrientation = 'horizontal';
+
+const _height = height || defaultHeight;
+const _color = color || defaultColor;
+const _orientation = orientation || defaultOrientation;
 </script>
 
-<style  lang="css" scoped>
+<style scoped lang="css">
 .divider {
-  width: 100%;
-  height: 1px;
-  background-color: var(--surface-subtle);
   margin: 1rem 0;
 }
 
+.horizontal {
+  width: 100%;
+  height: var(--height, 1px);
+}
+
+.vertical {
+  height: 100%;
+  width: var(--height, 1px);
+}
 </style>
