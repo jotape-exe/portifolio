08245b07b7bed9822f054003cfed7e45554c4ae5
João Pedro <joaoxstone@gmail.com>
2025-06-20 12:05:13 -0300

    i18n

---
+++ i18n.config.ts
@@ -0,0 +1,21 @@
+import ptBR from "./locales/br.json";
+import en from "./locales/en.json";
+import es from "./locales/es.json";
+
+export const languages = [
+  { language: "pt-BR", title: "Portugês BR" },
+  { language: "en-US", title: "English" },
+  { language: "es-ES", title: "Spanish" }
+];
+
+const messages = {
+  "en-US": en,
+  "pt-BR": ptBR,
+  "es-ES": es
+};
+
+export default defineI18nConfig(() => ({
+  legacy: false,
+  locale: "pt-BR",
+  messages: messages,
+}));
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', 'motion-v/nuxt'],
+  modules: ['@nuxt/icon', 'motion-v/nuxt', '@nuxtjs/i18n'],
   css: ['~/assets/css/main.css','~/assets/css/fonts.css', '~/assets/css/colors.css', '~/assets/css/design-system.css'],
   app: {
     head: {
